0863d5e06de9786d06173de7b61ead1106d8384b
Rajat Bhawsar <rajatbhawsar@Rajats-MacBook-Air.local>
2026-05-26 23:03:36 +0530

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mlops
+https://ml-ops.org/content/end-to-end-ml-workflow
---
+++ src/train.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import yaml
+import mlflow
+import joblib
+mlflow.sklearn.autolog()
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+# Load params
+with open("params.yaml") as f:
+    params = yaml.safe_load(f)
+
+max_depth = params["model"]["max_depth"]
+
+# Load data
+X_train = pd.read_csv("data/processed/X_train.csv")
+y_train = pd.read_csv("data/processed/y_train.csv")
+
+# Start MLflow
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("Titanic_MLOps")
+
+with mlflow.start_run():
+
+    # Train model
+    model = DecisionTreeClassifier(max_depth=max_depth)
+
+    model.fit(X_train, y_train.values.ravel())
+
+    # Log params
+    mlflow.log_param("max_depth", max_depth)
+
+    # Save model
+    joblib.dump(model, "models/model.pkl")
+
+    # Log artifact
+    mlflow.log_artifact("models/model.pkl")
+
+print("Model trained successfully")
